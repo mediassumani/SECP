0081d1953fa0c292eb9070d52206c27ab42f2352
Medi Assumani <mediassumani49@gmail.com>
2018-10-15 10:19:33 -0700

    First commit of my creation...SECP V1.0

---
+++ SECP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SECP
+//
+//  Created by Medi Assumani on 10/15/18.
+//  Copyright © 2018 Medi Assumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
